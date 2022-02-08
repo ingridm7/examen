14654e07fefb5580e346eef2c264be3118c7e624
ingridm7 <ingrid.karen.7@gmail.com>
2022-02-07 22:34:08 -0400

    2do commit - API rest

--- src/main/java/bo/edu/uap/app/App.java
+++ src/main/java/bo/edu/uap/app/App.java
@@ -1,13 +1,21 @@
 package bo.edu.uap.app;
+import static spark.Spark.*;
 
+import java.io.IOException;
+import java.util.Properties;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        Properties prop = new Properties();
+        prop.load(App.class.getClassLoader().getResourceAsStream("vars.properties"));
+
+        port(3456);
+        get("entornos", (req, res) -> "En el ambiente ..." + prop.getProperty("VARIABLE1"));       
+        get("prueba", (req, res) -> "esta es una prueba");       
     }
 }
